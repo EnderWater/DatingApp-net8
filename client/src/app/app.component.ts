d85d3469835ce8b3f2e54646d5f914aba5f309b7
Connor Freeman <91228092+EnderWater@users.noreply.github.com>
2025-06-12 18:53:35 -0700

    End of section 6

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { NavComponent } from "./nav/nav.component";
 import { AccountService } from './_services/account.service';
 import { User } from './_interfaces/user';
 import { HomeComponent } from "./home/home.component";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavComponent, HomeComponent],
+  imports: [NavComponent, HomeComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -4,17 +4,22 @@ import { AccountService } from '../_services/account.service';
 import { Login } from '../_interfaces/login';
 import { createDefaultLogin } from '../_interfaces/factory';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [FormsModule, BsDropdownModule],
+  imports: [FormsModule, BsDropdownModule, RouterLink, RouterLinkActive, TitleCasePipe],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
   public accountService = inject(AccountService);
   public model: Login = createDefaultLogin();
+  private router = inject(Router);
+  private toastr = inject(ToastrService);
 
   ngOnInit() {
     const currentUser = this.accountService.currentUser();
@@ -26,12 +31,15 @@ export class NavComponent {
       next: () => {
         const currentUser = this.accountService.currentUser();
         this.model.username = currentUser ? currentUser.userName : '';
+        this.router.navigateByUrl('/members');
       },
-      error: error => console.log(error)
+      error: error => this.toastr.error(error.error)
     });
   }
 
   async logout() {
     this.accountService.logout();
+    this.model.password = '';
+    this.router.navigateByUrl('/');
   }
 }
